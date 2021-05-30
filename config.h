9ed0c285f76d79c947b8629daafb0478756afe22
2931511042@qq.com <2931511042@qq.com>
2021-05-30 12:43:49 +0800

    v2

--- config.h
+++ config.h
@@ -6,7 +6,7 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 static char *font = "Liberation Mono:pixelsize=15:antialias=true:autohint=true";
-static int borderpx = 2;
+static int borderpx = 0;
 
 /*
  * What program is execed by st depends of these precedence rules:
@@ -95,7 +95,7 @@ unsigned int tabspaces = 8;
 
 /* bg opacity */
 
-float alpha = 0.822;
+float alpha = 0.62;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
@@ -188,10 +188,12 @@ static uint forcemousemod = ShiftMask;
  */
 static MouseShortcut mshortcuts[] = {
 	/* mask                 button   function        argument       release */
+	{ 0,                    Button4, kscrollup,       {.i = 1} },
+	{ 0,                    Button5, kscrolldown,       {.i = 1} }, 
 	{ XK_ANY_MOD,           Button2, selpaste,       {.i = 0},      1 },
-	{ ShiftMask,            Button4, ttysend,        {.s = "\033[5;2~"} },
+	//{ ShiftMask,            Button4, ttysend,        {.s = "\033[5;2~"} },
 	{ XK_ANY_MOD,           Button4, ttysend,        {.s = "\031"} },
-	{ ShiftMask,            Button5, ttysend,        {.s = "\033[6;2~"} },
+	//{ ShiftMask,            Button5, ttysend,        {.s = "\033[6;2~"} },
 	{ XK_ANY_MOD,           Button5, ttysend,        {.s = "\005"} },
 };
 
@@ -225,8 +227,8 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_Y,           selpaste,       {.i =  0} },
 	{ ShiftMask,            XK_Insert,      selpaste,       {.i =  0} },
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
-	{ ShiftMask,            XK_Page_Up,     kscrollup,      {.i = -1} },
-	{ ShiftMask,            XK_Page_Down,   kscrolldown,    {.i = -1} },
+	{ 0,            XK_Page_Up,     kscrollup,      {.i = -1} },
+	{ 0,            XK_Page_Down,   kscrolldown,    {.i = -1} },
 };
 
 /*
